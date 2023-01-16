cf26670bf0d278f8c8168ee17e58da70625ce696
sighphyre <liquidwicked64@gmail.com>
2023-01-16 13:58:55 +0200

    trim out ruby_sdk folder

--- ruby_sdk/lib/ruby_sdk.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "ruby_sdk/version"
-
-module RubySdk
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- ruby_sdk/lib/ruby_sdk/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module RubySdk
-  VERSION = "0.1.0"
-end
