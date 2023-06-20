19aa4e81bc429624396251a10c7d09a12a529df9
sighphyre <liquidwicked64@gmail.com>
2023-06-20 10:18:31 +0200

    chore: document safety expections for ffi layer

--- yggdrasilffi/src/lib.rs
+++ yggdrasilffi/src/lib.rs
@@ -50,8 +50,14 @@ pub extern "C" fn engine_new() -> *mut libc::c_void {
     Box::into_raw(Box::new(state)) as *mut libc::c_void
 }
 
+/// Frees the memory allocated for the engine
+/// # Safety
+///
+/// The caller is responsible for ensuring the pointer to the engine is a valid pointer
+/// This function must be called correctly to deallocate the memory allocated for the engine in
+/// the engine_new function
 #[no_mangle]
-pub extern "C" fn engine_free(ptr: *mut libc::c_void) {
+pub unsafe extern "C" fn engine_free(ptr: *mut libc::c_void) {
     if ptr.is_null() {
         return;
     }
@@ -60,8 +66,17 @@ pub extern "C" fn engine_free(ptr: *mut libc::c_void) {
     }
 }
 
+/// Takes a JSON string representing a set of toggles and returns a JSON string representing
+/// the metrics that should be sent to the server
+/// # Safety
+///
+/// The caller is responsible for ensuring the pointer to the engine is a valid pointer
+/// and that the engine is not dropped while the pointer is still in use.
 #[no_mangle]
-pub extern "C" fn engine_take_state(ptr: *mut libc::c_void, json: *const c_char) -> *const c_char {
+pub unsafe extern "C" fn engine_take_state(
+    ptr: *mut libc::c_void,
+    json: *const c_char,
+) -> *const c_char {
     let state = unsafe {
         assert!(!ptr.is_null());
         &mut *(ptr as *mut EngineState)
@@ -91,8 +106,14 @@ fn take_state(state: &mut EngineState, c_str: &CStr) -> Result<*mut i8, FFIError
     Ok(c_string.into_raw())
 }
 
+/// Returns a boolean representing the state of the requested toggle for the given context
+/// # Safety
+///
+/// The caller is responsible for ensuring the pointer to the engine is a valid pointer
+/// and that the engine is not dropped while the pointer is still in use.
+/// toggle_name and context must be pointers to valid UTF-8 strings.
 #[no_mangle]
-pub extern "C" fn engine_is_enabled(
+pub unsafe extern "C" fn engine_is_enabled(
     ptr: *mut libc::c_void,
     toggle_name: *const c_char,
     context: *const c_char,
@@ -128,11 +149,18 @@ fn is_enabled(
 ) -> Result<bool, FFIError> {
     let toggle_name: &str = c_toggle_name.to_str()?;
     let context_str: &str = c_context.to_str()?;
-    let context: Context = serde_json::from_str(&context_str)?;
+    let context: Context = serde_json::from_str(context_str)?;
 
-    Ok(state.is_enabled(&toggle_name, &context))
+    Ok(state.is_enabled(toggle_name, &context))
 }
 
+/// Resolves a variant from the underlying Yggdrasil engine
+/// # Safety
+///
+/// The caller is responsible for ensuring the pointer to the engine is a valid pointer
+/// Note that this function allocates memory on the heap and returns a pointer to it.
+/// The caller should ensure that engine_free_variant_def is called once that memory
+/// has been read by the caller
 #[no_mangle]
 pub unsafe extern "C" fn engine_get_variant(
     ptr: *mut libc::c_void,
@@ -159,13 +187,20 @@ fn get_variant(
 ) -> Result<CString, FFIError> {
     let toggle_name = c_toggle_name.to_str()?;
     let context_str: &str = c_context_str.to_str()?;
-    let context: Context = serde_json::from_str(&context_str)?;
+    let context: Context = serde_json::from_str(context_str)?;
 
     let variant = state.get_variant(toggle_name, &context);
     let json = serde_json::to_string(&variant)?;
     Ok(CString::new(json)?)
 }
 
+/// Destroys a variant definition, this should only be called on a pointer returned by
+/// engine_get_variant_def
+///
+/// # Safety
+/// The caller is responsible for ensuring the pointer to the variant definition is a valid pointer
+/// This function must be called on every pointer returned by engine_get_variant_def otherwise this
+/// will leak memory
 #[no_mangle]
 pub unsafe extern "C" fn engine_free_variant_def(ptr: *mut c_char) {
     if ptr.is_null() {
