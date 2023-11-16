3c6fa6de4abf2fc93f6d7ed318437bdce255a7de
sighphyre <liquidwicked64@gmail.com>
2023-11-16 12:07:20 +0200

    chore(dotnet): apply format

--- dotnet-engine/Yggdrasil.Engine/Types.cs
+++ dotnet-engine/Yggdrasil.Engine/Types.cs
@@ -4,96 +4,96 @@ namespace Yggdrasil;
 
 public class Context
 {
-  public string? UserId { get; set; }
-  public string? SessionId { get; set; }
-  public string? RemoteAddress { get; set; }
-  public string? Environment { get; set; }
-  public string? AppName { get; set; }
-  public DateTimeOffset? CurrentTime { get; set; }
-  public Dictionary<string, string>? Properties { get; set; }
+    public string? UserId { get; set; }
+    public string? SessionId { get; set; }
+    public string? RemoteAddress { get; set; }
+    public string? Environment { get; set; }
+    public string? AppName { get; set; }
+    public DateTimeOffset? CurrentTime { get; set; }
+    public Dictionary<string, string>? Properties { get; set; }
 }
 
 public class EngineResponse
 {
-  [JsonPropertyName("error_message")]
-  public string? ErrorMessage { get; set; }
+    [JsonPropertyName("error_message")]
+    public string? ErrorMessage { get; set; }
 
-  [JsonPropertyName("status_code")]
-  public string? StatusCode { get; set; }
+    [JsonPropertyName("status_code")]
+    public string? StatusCode { get; set; }
 }
 
 public class EngineResponse<TValue> : EngineResponse
 {
-  public TValue? Value { get; set; }
+    public TValue? Value { get; set; }
 }
 
 public class Variant
 {
-  public Variant(string name, Payload? payload, bool enabled)
-  {
-    Name = name;
-    Payload = payload;
-    Enabled = enabled;
-  }
+    public Variant(string name, Payload? payload, bool enabled)
+    {
+        Name = name;
+        Payload = payload;
+        Enabled = enabled;
+    }
 
-  public string Name { get; set; }
-  public Payload? Payload { get; set; }
-  public bool Enabled { get; set; }
+    public string Name { get; set; }
+    public Payload? Payload { get; set; }
+    public bool Enabled { get; set; }
 }
 
 public class Payload
 {
-  public string? PayloadType { get; set; }
-  public string? Value { get; set; }
+    public string? PayloadType { get; set; }
+    public string? Value { get; set; }
 
-  public override bool Equals(object? obj)
-  {
-    if (obj == null)
-      return false;
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
 
-    var payload = (Payload)obj;
-    return Value == payload.Value && PayloadType == payload.PayloadType;
-  }
+        var payload = (Payload)obj;
+        return Value == payload.Value && PayloadType == payload.PayloadType;
+    }
 
-  public override int GetHashCode()
-  {
-    return HashCode.Combine(Value, PayloadType);
-  }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Value, PayloadType);
+    }
 }
 
 public class YggdrasilEngineException : Exception
 {
-  public YggdrasilEngineException(string message) : base(message) { }
+    public YggdrasilEngineException(string message) : base(message) { }
 }
 
 public class FeatureCount
 {
-  public FeatureCount(long yes, long no, Dictionary<string, long> variants)
-  {
-    Yes = yes;
-    No = no;
-    Variants = variants;
-  }
+    public FeatureCount(long yes, long no, Dictionary<string, long> variants)
+    {
+        Yes = yes;
+        No = no;
+        Variants = variants;
+    }
 
-  public long Yes { get; set; }
-  public long No { get; set; }
-  public Dictionary<string, long> Variants { get; set; }
+    public long Yes { get; set; }
+    public long No { get; set; }
+    public Dictionary<string, long> Variants { get; set; }
 }
 
 
 public class MetricsBucket
 {
-  public MetricsBucket(Dictionary<string, FeatureCount> toggles, DateTimeOffset start, DateTimeOffset stop)
-  {
-    Toggles = toggles;
-    Start = start;
-    Stop = stop;
-  }
+    public MetricsBucket(Dictionary<string, FeatureCount> toggles, DateTimeOffset start, DateTimeOffset stop)
+    {
+        Toggles = toggles;
+        Start = start;
+        Stop = stop;
+    }
 
-  public Dictionary<string, FeatureCount> Toggles { get; set; }
+    public Dictionary<string, FeatureCount> Toggles { get; set; }
 
-  public DateTimeOffset Start { get; set; }
-  public DateTimeOffset Stop { get; set; }
+    public DateTimeOffset Start { get; set; }
+    public DateTimeOffset Stop { get; set; }
 }
 
 /// <summary>
@@ -101,67 +101,67 @@ public class MetricsBucket
 /// </summary>
 public interface IStrategy
 {
-  /// <summary>
-  /// Gets the stragegy name 
-  /// </summary>
-  string Name { get; }
+    /// <summary>
+    /// Gets the stragegy name 
+    /// </summary>
+    string Name { get; }
 
-  /// <summary>
-  /// Calculates if the strategy is enabled for a given context
-  /// </summary>
-  bool IsEnabled(Dictionary<string, string> parameters, Context context);
+    /// <summary>
+    /// Calculates if the strategy is enabled for a given context
+    /// </summary>
+    bool IsEnabled(Dictionary<string, string> parameters, Context context);
 }
 
 class StrategyDefinition
 {
-  public string Name { get; set; } = "";
+    public string Name { get; set; } = "";
 
-  public Dictionary<string, string>? Parameters { get; set; }
+    public Dictionary<string, string>? Parameters { get; set; }
 }
 
 class Feature
 {
-  public string Name { get; set; } = "";
-  public List<StrategyDefinition>? Strategies { get; set; }
+    public string Name { get; set; } = "";
+    public List<StrategyDefinition>? Strategies { get; set; }
 }
 
 class FeatureCollection
 {
-  public List<Feature>? Features { get; set; }
+    public List<Feature>? Features { get; set; }
 }
 
 class MappedFeature
 {
-  public MappedFeature(Feature feature, List<MappedStrategy> strategies)
-  {
-    Name = feature.Name;
-    Strategies = strategies;
-  }
+    public MappedFeature(Feature feature, List<MappedStrategy> strategies)
+    {
+        Name = feature.Name;
+        Strategies = strategies;
+    }
 
-  public string Name { get; }
-  public List<MappedStrategy> Strategies { get; }
+    public string Name { get; }
+    public List<MappedStrategy> Strategies { get; }
 }
 
 class MappedStrategy
 {
-  public MappedStrategy(int index, string strategyName, Dictionary<string, string> parameters, IStrategy strategy)
-  {
-    ResultName = $"customStrategy{index + 1}";
-    StrategyName = strategyName;
-    Strategy = strategy;
-    Parameters = parameters;
-  }
+    public MappedStrategy(int index, string strategyName, Dictionary<string, string> parameters, IStrategy strategy)
+    {
+        ResultName = $"customStrategy{index + 1}";
+        StrategyName = strategyName;
+        Strategy = strategy;
+        Parameters = parameters;
+    }
 
-  public string ResultName { get; private set; }
+    public string ResultName { get; private set; }
 
-  public string StrategyName { get; private set; }
+    public string StrategyName { get; private set; }
 
-  public IStrategy Strategy { get; private set; }
+    public IStrategy Strategy { get; private set; }
 
-  public Dictionary<string, string> Parameters { get; private set; }
+    public Dictionary<string, string> Parameters { get; private set; }
 
-  public bool IsEnabled(Context context)
-  {
-    return Strategy.IsEnabled(Parameters, context);
-  }
+    public bool IsEnabled(Context context)
+    {
+        return Strategy.IsEnabled(Parameters, context);
+    }
 }
