caa0355c60934ca45c091fd80843a9ca8a751500
Nuno Góis <github@nunogois.com>
2024-06-20 14:03:57 +0100

    try PtrToStringAnsi

--- dotnet-engine/Yggdrasil.Engine/FFIReader.cs
+++ dotnet-engine/Yggdrasil.Engine/FFIReader.cs
@@ -94,7 +94,7 @@ public static class FFIReader
 
         try
         {
-            var json = Marshal.PtrToStringUni(ptr);
+            var json = Marshal.PtrToStringAnsi(ptr);
 
             var result = json != null ? JsonSerializer.Deserialize<T>(json, options) : null;
 
