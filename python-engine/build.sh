1eeb5ccb9ec0e9fd9bf59b1fe10b05db4bb98e42
Simon Hornby <liquidwicked64@gmail.com>
2024-11-07 14:32:21 +0200

    chore: build for windows + mac too

--- python-engine/build.sh
+++ python-engine/build.sh
@@ -1,3 +1,6 @@
 cargo build --release
 mkdir -p lib
-cp ../target/release/libyggdrasilffi.so ./lib/
+
+[ -f ../target/release/yggdrasilffi.dll ] && cp ../target/release/yggdrasilffi.dll ./lib
+[ -f ../target/release/libyggdrasilffi.dylib ] && cp ../target/release/libyggdrasilffi.dylib ./lib
+[ -f ../target/release/libyggdrasilffi.so ] && cp ../target/release/libyggdrasilffi.so ./lib
