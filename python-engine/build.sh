7c391b90614e8fab60a30cad6f2bd030484bb61f
Simon Hornby <liquidwicked64@gmail.com>
2024-11-08 10:10:33 +0200

    fix: build paths for python build script

--- python-engine/build.sh
+++ python-engine/build.sh
@@ -1,6 +1,6 @@
 cargo build --release
-mkdir -p yggdrasil-engine/lib
+mkdir -p yggdrasil_engine/lib
 
-[ -f ../target/release/yggdrasilffi.dll ] && cp ../target/release/yggdrasilffi.dll ./yggdrasil-engine/lib
-[ -f ../target/release/libyggdrasilffi.dylib ] && cp ../target/release/libyggdrasilffi.dylib ./yggdrasil-engine/lib
-[ -f ../target/release/libyggdrasilffi.so ] && cp ../target/release/libyggdrasilffi.so ./yggdrasil-engine/lib
+[ -f ../target/release/yggdrasilffi.dll ] && cp ../target/release/yggdrasilffi.dll ./yggdrasil_engine/lib
+[ -f ../target/release/libyggdrasilffi.dylib ] && cp ../target/release/libyggdrasilffi.dylib ./yggdrasil_engine/lib
+[ -f ../target/release/libyggdrasilffi.so ] && cp ../target/release/libyggdrasilffi.so ./yggdrasil_engine/lib
