4aefbea3da3210250a31fa537c67fa05f56e4b46
Simon Hornby <liquidwicked64@gmail.com>
2024-11-19 14:52:23 +0200

    chore: reenable sonatype publush

--- java-engine/build.gradle.kts
+++ java-engine/build.gradle.kts
@@ -96,7 +96,16 @@ publishing {
     }
 }
 
-
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(sonatypeUsername)
+            password.set(sonatypePassword)
+        }
+    }
+}
 
 java {
   withSourcesJar()
