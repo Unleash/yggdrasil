7127e517a68c39ca69bbd8b4fb9a9d81a8088d23
Simon Hornby <liquidwicked64@gmail.com>
2025-01-13 14:08:17 +0200

    chore: remove errant system out

--- java-engine/src/main/java/io/getunleash/engine/UnleashEngine.java
+++ java-engine/src/main/java/io/getunleash/engine/UnleashEngine.java
@@ -168,7 +168,6 @@ public class UnleashEngine {
     @Override
     protected void finalize() {
         if (cleanerIsSupported()) {
-            System.out.println("Finalize dodged");
             return;
         }
         try {
