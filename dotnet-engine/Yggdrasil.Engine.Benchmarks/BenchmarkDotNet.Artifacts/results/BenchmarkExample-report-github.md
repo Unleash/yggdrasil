f30dcd3632128ae3a5ff9cbfa22f87410806503a
Simon Hornby <liquidwicked64@gmail.com>
2025-01-30 09:54:22 +0200

    chore: barebones empty is enabled benchmark

---
+++ dotnet-engine/Yggdrasil.Engine.Benchmarks/BenchmarkDotNet.Artifacts/results/BenchmarkExample-report-github.md
@@ -0,0 +1,13 @@
+```
+
+BenchmarkDotNet v0.14.0, Ubuntu 22.04.5 LTS (Jammy Jellyfish)
+13th Gen Intel Core i7-13700HX, 1 CPU, 24 logical and 16 physical cores
+.NET SDK 8.0.112
+  [Host]     : .NET 8.0.12 (8.0.1224.60305), X64 RyuJIT AVX2
+  DefaultJob : .NET 8.0.12 (8.0.1224.60305), X64 RyuJIT AVX2
+
+
+```
+| Method    | Mean     | Error     | StdDev    | Median   |
+|---------- |---------:|----------:|----------:|---------:|
+| SortArray | 3.909 μs | 0.2931 μs | 0.8641 μs | 3.622 μs |
--- dotnet-engine/Yggdrasil.Engine.Benchmarks/Program.cs
+++ dotnet-engine/Yggdrasil.Engine.Benchmarks/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Yggdrasil;
+
+public class EngineBenchmarks
+{
+    private YggdrasilEngine engine;
+    private Context context;
+
+    public EngineBenchmarks()
+    {
+        engine = new YggdrasilEngine();
+        context = new Context();
+    }
+
+    [Benchmark]
+    public void EmptyIsEnabled() => engine.IsEnabled("missing", context);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<EngineBenchmarks>();
+    }
+}
