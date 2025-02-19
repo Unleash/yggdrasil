7f4f238f7e8d5a36c34e8fac3bce5fbdb5d2ac7d
Simon Hornby <liquidwicked64@gmail.com>
2025-02-19 13:31:40 +0200

    chore: remove nexus url

--- java-engine/build.gradle.kts
+++ java-engine/build.gradle.kts
@@ -129,17 +129,6 @@ publishing {
     }
 }
 
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(sonatypeUsername)
-            password.set(sonatypePassword)
-        }
-    }
-}
-
 java {
     withSourcesJar()
     withJavadocJar()
