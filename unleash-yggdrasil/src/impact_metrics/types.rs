1c6fdf95f3900d442d9690f9df9bcaa37def25df
Jaanus <sellinjaanus@gmail.com>
2026-01-15 14:14:20 +0200

    Fix

--- unleash-yggdrasil/src/impact_metrics/types.rs
+++ unleash-yggdrasil/src/impact_metrics/types.rs
@@ -84,59 +84,25 @@ where
     }
 }
 
-fn deserialize_le<'de, D>(deserializer: D) -> Result<f64, D::Error>
+fn deserialize_le<'de, D>(d: D) -> Result<f64, D::Error>
 where
     D: serde::Deserializer<'de>,
 {
-    use serde::de::{self, Visitor};
-    use std::fmt;
-
-    struct LeVisitor;
-
-    impl<'de> Visitor<'de> for LeVisitor {
-        type Value = f64;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("a number or '+Inf' string")
-        }
-
-        fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
-        where
-            E: de::Error,
-        {
-            Ok(v)
-        }
-
-        fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
-        where
-            E: de::Error,
-        {
-            Ok(v as f64)
-        }
-
-        fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
-        where
-            E: de::Error,
-        {
-            Ok(v as f64)
-        }
-
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: de::Error,
-        {
-            if v == "+Inf" {
-                Ok(f64::INFINITY)
-            } else {
-                Err(de::Error::invalid_value(
-                    de::Unexpected::Str(v),
-                    &"a number or '+Inf' string",
-                ))
-            }
-        }
+    #[derive(serde::Deserialize)]
+    #[serde(untagged)]
+    enum F64OrInf {
+        Num(f64),
+        Str(String),
     }
 
-    deserializer.deserialize_any(LeVisitor)
+    match F64OrInf::deserialize(d)? {
+        F64OrInf::Num(x) => Ok(x),
+        F64OrInf::Str(s) if s == "+Inf" => Ok(f64::INFINITY),
+        F64OrInf::Str(s) => Err(serde::de::Error::invalid_value(
+            serde::de::Unexpected::Str(&s),
+            &"a number or '+Inf' string",
+        )),
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
